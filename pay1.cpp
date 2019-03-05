1f1f361d36bd218a22f63d8196ee60249bbf1cc7
Abeeaad97 <abidbakhtiyar@csu.fullerton.edu>
2019-03-05 11:08:11 -0800

    Added if statement to readData()

--- pay1.cpp
+++ pay1.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <string>
-#include "person1.cpp"
+#include "person.cpp"
 #include <fstream>
 #include <cstring>
 using namespace std;
 
-void readData(Person per[20]);
-void writeData(Person per[20]);
+int readData(Person per[20]);
+void writeData(Person per[20], int size);
 int main()
 {
+    int size;
     Person per[20];
-    readData(per);
-    writeData(per);
+    size = readData(per);
+    writeData(per, size);
 
     cout << "done";
 
 
 }
-void readData(Person per[20])
+int readData(Person per[20])
 {
+    int num;
     string fname;
     string lname;
     float pay;
@@ -28,7 +30,6 @@ void readData(Person per[20])
 
     for(int i = 0; i < 20; i++)
     {
-
         inData >> fname;
         inData >> lname;
         inData >> pay;
@@ -37,15 +38,24 @@ void readData(Person per[20])
         per[i].setLastName(lname);
         per[i].setPayRate(pay);
         per[i].setHoursWorked(hours);
+        if(!inData.eof())
+        {
+            num++;
+        }
+        
+
+        
     }
+    //num = num - 1;
+    return num;
 }
-void writeData(Person p[20])
+void writeData(Person p[20], int size)
 {
     float totPay;
     string full;
     ofstream outData;
     outData.open("output.txt");
-    for(int i = 0; i < 6; i++)
+    for(int i = 0; i < size; i++)
     {
 
         full = p[i].fullName();
